753cd61e3d34b305a2ff54a04731e1775f18ebea
Francisco <public@francisco.io>
2026-09-06 00:06:38 +0900

    Credit the author on the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,9 @@ export default () => (
       </Header>
       <List />
       <AddItem />
+      <footer style={{ textAlign: "center", padding: 20, opacity: 0.7, fontSize: 13 }}>
+        Made by <a href="https://francisco.io/">Francisco</a>.
+      </footer>
     </Main>
   </Store>
 );
